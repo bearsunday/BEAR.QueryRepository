ce83c9212faf3ca0e75e0dd0ff755f3704aa50f1
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-04-18 12:43:46 +0900

    update demo code

--- docs/demo/AppModule.php
+++ docs/demo/AppModule.php
@@ -5,6 +5,7 @@ namespace FakeVendor\DemoApp;
 use BEAR\QueryRepository\QueryRepositoryModule;
 use BEAR\RepositoryModule\Annotation\Storage;
 use BEAR\Resource\Module\ResourceModule;
+use Doctrine\Common\Cache\ArrayCache;
 use Doctrine\Common\Cache\CacheProvider;
 use Doctrine\Common\Cache\FilesystemCache;
 use Ray\Di\AbstractModule;
@@ -18,7 +19,8 @@ class AppModule extends AbstractModule
     protected function configure()
     {
         $this->bind()->annotatedWith("storage_dir")->toInstance(__DIR__ . '/tmp')->in(Scope::SINGLETON);
-        $this->bind(CacheProvider::class)->annotatedWith(Storage::class)->toConstructor(FilesystemCache::class, 'directory=storage_dir')->in(Scope::SINGLETON);
+//        $this->bind(CacheProvider::class)->annotatedWith(Storage::class)->toConstructor(FilesystemCache::class, 'directory=storage_dir')->in(Scope::SINGLETON);
+        $this->bind(CacheProvider::class)->annotatedWith(Storage::class)->to(ArrayCache::class)->in(Scope::SINGLETON);
         $this->install(new ResourceModule(__NAMESPACE__));
         $this->install(new QueryRepositoryModule);
     }
