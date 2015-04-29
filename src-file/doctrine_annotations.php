fe3a23549762c6d37763ea78874e01e3a5d3921e
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-04-29 22:14:33 +0900

    MIT license

--- src-file/doctrine_annotations.php
+++ src-file/doctrine_annotations.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the BEAR.QueryRepository package
  *
- * @license http://opensource.org/licenses/bsd-license.php BSD
+ * @license http://opensource.org/licenses/MIT MIT
  */
 require dirname(__DIR__) . '/src/Annotation/ExpiryConfig.php';
 require dirname(__DIR__) . '/src/Annotation/Cacheable.php';
--- src/Annotation/Storage.php
+++ src/Annotation/Storage.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the BEAR.QueryRepository package
  *
- * @license http://opensource.org/licenses/bsd-license.php BSD
+ * @license http://opensource.org/licenses/MIT MIT
  */
 namespace BEAR\RepositoryModule\Annotation;
 
