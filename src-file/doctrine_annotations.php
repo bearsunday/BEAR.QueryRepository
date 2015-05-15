bb8bbd81636b6e48e7474581f433395a24f38011
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-15 12:31:34 +0900

    add AbstractCommand annotation

--- src-file/doctrine_annotations.php
+++ src-file/doctrine_annotations.php
@@ -4,6 +4,7 @@
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
+require dirname(__DIR__) . '/src/Annotation/AbstractCommand.php';
 require dirname(__DIR__) . '/src/Annotation/ExpiryConfig.php';
 require dirname(__DIR__) . '/src/Annotation/Cacheable.php';
 require dirname(__DIR__) . '/src/Annotation/Purge.php';
