be62f07605902e0874609687c303be6356a3a932
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-19 18:05:08 +0900

    period at the end

--- src/Exception/UnmatchedQuery.php
+++ src/Exception/UnmatchedQuery.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the BEAR.QueryRepository package
+ * This file is part of the BEAR.QueryRepository package.
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
