c4f369d1b7f4d3e912c799d3e5ed5d0538e88ccd
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-19 21:38:57 +0900

    fix docblock

--- src/RefreshAnnotatedCommand.php
+++ src/RefreshAnnotatedCommand.php
@@ -77,8 +77,8 @@ class RefreshAnnotatedCommand implements CommandInterface
     }
 
     /**
-     * @param ResourceObject $resourceObject
-     * @param                $annotation
+     * @param ResourceObject  $resourceObject
+     * @param object          $annotation
      */
     private function request(ResourceObject $resourceObject, $annotation)
     {
