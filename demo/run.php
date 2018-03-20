c69c877485fb7fbb315e01ffd41bf3751f034aa7
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-20 19:08:08 +0900

    refactor demo code with php7 syntax sugar

--- demo/run.php
+++ demo/run.php
@@ -8,6 +8,16 @@ use BEAR\Resource\ResourceInterface;
 use FakeVendor\DemoApp\AppModule;
 use Ray\Di\Injector;
 
+function echoRo(\BEAR\Resource\ResourceObject $ro)
+{
+    echo $ro->uri->method . PHP_EOL;
+    echo $ro->code . PHP_EOL;
+    foreach ($ro->headers as $headerKey => $headerValue) {
+        echo "{$headerKey}:{$headerValue}" . PHP_EOL;
+    }
+    echo $ro . PHP_EOL;
+}
+
 /* @var $loader \Composer\Autoload\ClassLoader */
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 $loader->addPsr4('FakeVendor\DemoApp\\', __DIR__);
@@ -15,22 +25,12 @@ $loader->addPsr4('FakeVendor\DemoApp\\', __DIR__);
 /* @var $resource ResourceInterface */
 $resource = (new Injector(new AppModule, __DIR__ . '/tmp'))->getInstance(ResourceInterface::class);
 
-echo 'GET' . PHP_EOL;
-$user = $resource->get->uri('app://self/user')->withQuery(['id' => 1])->eager->request();
-echo $user->code . $user . PHP_EOL . PHP_EOL;
+echoRo($resource->uri('app://self/user')(['id' => 1])); // create cache
 
-echo 'GET' . PHP_EOL;
-$user = $resource->get->uri('app://self/user')->withQuery(['id' => 1])->eager->request();
-echo $user->code . $user . PHP_EOL . PHP_EOL;
+echoRo($resource->uri('app://self/user')(['id' => 1])); // return cache
 
-echo 'PATCH' . PHP_EOL;
-$user = $resource->patch->uri('app://self/user')->withQuery(['id' => 1, 'name' => 'kuma'])->eager->request();
-echo $user->code . $user . PHP_EOL . PHP_EOL;
+echoRo($resource->patch->uri('app://self/user')(['id' => 1, 'name' => 'kuma'])); // recreate cache
 
-echo 'GET' . PHP_EOL;
-$user = $resource->get->uri('app://self/user')->withQuery(['id' => 1])->eager->request();
-echo $user->code . $user . PHP_EOL . PHP_EOL;
+echoRo($resource->uri('app://self/user')(['id' => 1])); // return cache
 
-echo 'GET' . PHP_EOL;
-$user = $resource->get->uri('app://self/user')->withQuery(['id' => 1])->eager->request();
-echo $user->code . $user . PHP_EOL . PHP_EOL;
+echoRo($resource->uri('app://self/user')(['id' => 1])); // return cache
