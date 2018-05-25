c020927f409320ef276cc9e425096680ae08dcf9
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-05-25 19:00:59 +0900

    add RefreshCacheSrc

---
+++ tests/Fake/fake-app/src/Resource/App/RefreshCacheSrc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the BEAR.QueryRepository package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace FakeVendor\HelloWorld\Resource\App;
+
+use BEAR\RepositoryModule\Annotation\Refresh;
+use BEAR\Resource\ResourceObject;
+
+class RefreshCacheSrc extends ResourceObject
+{
+    public function onGet($id)
+    {
+        return $this;
+    }
+    
+    /**
+     * @Refresh(uri="app://self/refresh-dest{?id}")
+     */
+    public function onPut($id)
+    {
+        unset($id);
+
+        return $this;
+    }
+}
