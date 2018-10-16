60010e0f4d29946524828b149a975a8782e66670
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-10-17 03:19:25 +0900

    move deprecated src

---
+++ src-deprecated/HttpCacheInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\QueryRepository;
+
+use BEAR\Sunday\Extension\Transfer\HttpCacheInterface as OriginHttpCacheInterface;
+
+/**
+ * @deprecated Use \BEAR\Sunday\Extension\Transfer\HttpCacheInterface instead
+ */
+interface HttpCacheInterface extends OriginHttpCacheInterface
+{
+}
--- src/HttpCacheInject.php
+++ src/HttpCacheInject.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
+use BEAR\Sunday\Extension\Transfer\HttpCacheInterface;
+
 trait HttpCacheInject
 {
     /**
