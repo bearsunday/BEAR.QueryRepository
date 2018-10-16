e5c6bffb7ae2203cb624843b85e7550aaa56f61f
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-10-17 03:55:59 +0900

    revert instanceof check

--- src/CacheInterceptor.php
+++ src/CacheInterceptor.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
-use BEAR\QueryRepository\Exception\ReturnValueIsNotResourceObjectException;
 use BEAR\Resource\ResourceObject;
 use Ray\Aop\MethodInterceptor;
 use Ray\Aop\MethodInvocation;
-use function get_class;
 
 class CacheInterceptor implements MethodInterceptor
 {
@@ -28,10 +26,8 @@ class CacheInterceptor implements MethodInterceptor
      */
     public function invoke(MethodInvocation $invocation)
     {
+        /** @var ResourceObject $ro */
         $ro = $invocation->getThis();
-        if (! $ro instanceof ResourceObject) {
-            throw new ReturnValueIsNotResourceObjectException(get_class($ro));
-        }
         $stored = $this->repository->get($ro->uri);
         if ($stored) {
             list($ro->uri, $ro->code, $ro->headers, $ro->body, $ro->view) = $stored;
