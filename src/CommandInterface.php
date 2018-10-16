81b087d709638964b85c38a6ed136f631eb41fa2
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-10-16 23:50:01 +0900

    add phpdoc

--- src/CommandInterface.php
+++ src/CommandInterface.php
@@ -9,6 +9,11 @@ namespace BEAR\QueryRepository;
 use BEAR\Resource\ResourceObject;
 use Ray\Aop\MethodInvocation;
 
+/**
+ * Command
+ *
+ * Change resource state
+ */
 interface CommandInterface
 {
     public function command(MethodInvocation $invocation, ResourceObject $ro);
