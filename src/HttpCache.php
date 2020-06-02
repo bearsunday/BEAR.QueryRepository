7f75cecd1db6aff2fabdcc87f73a951b035b34d2
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-06-02 11:05:49 +0900

    fix DperecatedInterface

--- src/HttpCache.php
+++ src/HttpCache.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
+use BEAR\Sunday\Extension\Transfer\HttpCacheInterface;
+
 final class HttpCache implements HttpCacheInterface
 {
     /**
