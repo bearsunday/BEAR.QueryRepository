ccce2d929a5b000de9bf13fcb98395c50ddecade
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-07-16 09:53:20 +0900

    Move test-deprecated

---
+++ test-deprecated/StorageApcModuleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\QueryRepository;
+
+use BEAR\RepositoryModule\Annotation\Storage;
+use Doctrine\Common\Cache\ApcuCache;
+use Doctrine\Common\Cache\CacheProvider;
+use PHPUnit\Framework\TestCase;
+use Ray\Di\Injector;
+
+class StorageApcModuleTest extends TestCase
+{
+    public function testNew(): void
+    {
+        $cache = (new Injector(new StorageApcModule(), $_ENV['TMP_DIR']))->getInstance(CacheProvider::class, Storage::class);
+        $this->assertInstanceOf(ApcuCache::class, $cache);
+    }
+}
