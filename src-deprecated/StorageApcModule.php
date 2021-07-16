56ef226e5613740a6edfb9c06dc3f324d1aa2b4a
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-07-16 09:45:26 +0900

    Optimize imports

--- src-deprecated/StorageApcModule.php
+++ src-deprecated/StorageApcModule.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
-use BEAR\RepositoryModule\Annotation\CacheEngine;
-use BEAR\RepositoryModule\Annotation\Storage;
-use Doctrine\Common\Cache\ApcuCache;
-use Doctrine\Common\Cache\CacheProvider;
 use Ray\Di\AbstractModule;
-use Ray\Di\Scope;
+use Ray\PsrCacheModule\Psr6ApcuModule;
 
 /**
  * @deprecated
@@ -23,8 +19,6 @@ class StorageApcModule extends AbstractModule
      */
     protected function configure()
     {
-        /** @psalm-suppress DeprecatedClass */
-        $this->bind(CacheProvider::class)->annotatedWith(CacheEngine::class)->to(ApcuCache::class);
-        $this->bind(CacheProvider::class)->annotatedWith(Storage::class)->toProvider(NamespacedCacheProvider::class)->in(Scope::SINGLETON);
+        $this->install(new Psr6ApcuModule());
     }
 }
--- src/Exception/RedisConnectionException.php
+++ src/Exception/RedisConnectionException.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository\Exception;
 
-use RuntimeException;
-
 class RedisConnectionException extends RuntimeException
 {
 }
