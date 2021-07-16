eb9a08249e5e47e28d768b66ab2fdfe32cae9cf5
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-07-16 09:27:11 +0900

    Keep BC of CacheVersionModule

--- tests/CacheVersionModuleTest.php
+++ tests/CacheVersionModuleTest.php
@@ -4,28 +4,18 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
-use BEAR\RepositoryModule\Annotation\Storage;
-use BEAR\Resource\Module\ResourceModule;
-use Doctrine\Common\Cache\Cache;
-use Doctrine\Common\Cache\CacheProvider;
 use PHPUnit\Framework\TestCase;
 use Ray\Di\Injector;
-use Ray\Di\NullModule;
+use Ray\PsrCacheModule\Annotation\CacheNamespace;
 
 class CacheVersionModuleTest extends TestCase
 {
     public function testNew(): void
     {
-        $namespace = 'FakeVendor\HelloWorld';
         $version = '1';
-        $module = new NullModule();
-        $module->install(new CacheVersionModule($version));
-        $module->install(new QueryRepositoryModule(new ResourceModule($namespace)));
+        $module = new CacheVersionModule($version);
         $injector = new Injector($module, $_ENV['TMP_DIR']);
-        $cache = $injector->getInstance(Cache::class, Storage::class);
-        /** @var CacheProvider $cache */
-        $ns = $cache->getNamespace();
-        $expected = $namespace . $version;
-        $this->assertSame($expected, $ns);
+        $ns = $injector->getInstance('', CacheNamespace::class);
+        $this->assertSame($version, $ns);
     }
 }
