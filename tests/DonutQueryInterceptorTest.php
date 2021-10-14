170b59dbc11aed9ff53ae23ec2d607ad42308abc
koriym <akihito.koriyama@gmail.com>
2021-10-14 11:46:21 +0900

    Add testCached log

--- tests/DonutQueryInterceptorTest.php
+++ tests/DonutQueryInterceptorTest.php
@@ -20,19 +20,22 @@ class DonutQueryInterceptorTest extends TestCase
     /** @var ResourceInterface */
     private $resource;
 
+    /** @var RepositoryLoggerInterface */
+    private $logger;
+
     protected function setUp(): void
     {
         static $injector;
 
         $namespace = 'FakeVendor\HelloWorld';
-        $module = new FakeEtagPoolModule(new QueryRepositoryModule(new ResourceModule($namespace)));
+        $module = new DevEtagModule((new FakeEtagPoolModule(new QueryRepositoryModule(new ResourceModule($namespace)))));
         $module->override(new TwigModule([dirname(__DIR__) . '/tests/Fake/fake-app/var/templates']));
         if (! $injector) {
             $injector = new Injector($module, $_ENV['TMP_DIR']);
         }
 
         $this->resource = $injector->getInstance(ResourceInterface::class);
-
+        $this->logger = $injector->getInstance(RepositoryLoggerInterface::class);
         parent::setUp();
     }
 
@@ -66,8 +69,20 @@ class DonutQueryInterceptorTest extends TestCase
     public function testCached(): void
     {
         // test cached
+        $this->logger->log('get');
         $blogPosting = $this->resource->get('page://self/html/blog-posting');
         assert($blogPosting instanceof BlogPosting);
+        $log = (string) $this->logger;
+        $this->assertSame('no-donut-found uri:page://self/html/blog-posting
+create-donut: uri:page://self/html/blog-posting ttl: s-maxage:0
+put-query-repository uri:page://self/html/comment
+update-etag uri:page://self/html/comment etag:_html_comment_ surrogate-keys:
+save-value uri:page://self/html/comment ttl:31536000
+save-donut uri:page://self/html/blog-posting s-maxage:
+save-view uri:page://self/html/blog-posting ttl:0
+update-etag uri:page://self/html/blog-posting etag:_html_blog-posting_ surrogate-keys:_html_comment_
+get
+get-donut-cache: uri:page://self/html/blog-posting', $log);
         $this->assertArrayHasKey('Age', $blogPosting->headers);
         $this->assertArrayHasKey(Header::CDN_CACHE_CONTROL, $blogPosting->headers);
     }
