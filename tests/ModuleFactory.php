d88e63b3436080ff5aed367fc8d8115d5ffce853
koriym <akihito.koriyama@gmail.com>
2021-11-06 20:46:52 +0900

    Bind default EtagPool

--- tests/ModuleFactory.php
+++ tests/ModuleFactory.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
+use BEAR\RepositoryModule\Annotation\EtagPool;
 use BEAR\Resource\Module\ResourceModule;
-use Ray\PsrCacheModule\Psr6ArrayModule;
+use Psr\Cache\CacheItemPoolInterface;
+use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
+use Ray\PsrCacheModule\Annotation\Shared;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 final class ModuleFactory
 {
     public static function getInstance(string $namespace): QueryRepositoryModule
     {
         $module = new QueryRepositoryModule(new ResourceModule($namespace));
-        $module->override(new Psr6ArrayModule());
+        $module->override(new class extends AbstractModule{
+            protected function configure(): void
+            {
+                $this->bind(CacheItemPoolInterface::class)->annotatedWith(Shared::class)->to(ArrayAdapter::class)->in(Scope::SINGLETON);
+                $this->bind(CacheItemPoolInterface::class)->annotatedWith(EtagPool::class)->to(ArrayAdapter::class)->in(Scope::SINGLETON);
+            }
+        });
 
         return $module;
     }
