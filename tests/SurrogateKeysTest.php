526a7caa38523af2ac93aa07544116a78230ef6c
Yoshitaka Jingu <jingu@bitnauts.co.jp>
2022-07-20 17:28:01 +0900

    Added Surrogate-Key test to be set independently by Page resources

--- tests/SurrogateKeysTest.php
+++ tests/SurrogateKeysTest.php
@@ -4,12 +4,34 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
+use BEAR\Resource\ResourceInterface;
 use BEAR\Resource\ResourceObject;
 use BEAR\Resource\Uri;
+use Madapaja\TwigModule\TwigModule;
 use PHPUnit\Framework\TestCase;
+use Ray\Di\Injector;
+
+use function dirname;
 
 class SurrogateKeysTest extends TestCase
 {
+    private ResourceInterface $resource;
+
+    protected function setUp(): void
+    {
+        static $injector;
+
+        $namespace = 'FakeVendor\HelloWorld';
+        $module = new DevEtagModule((new FakeEtagPoolModule(ModuleFactory::getInstance($namespace))));
+        $module->override(new TwigModule([dirname(__DIR__) . '/tests/Fake/fake-app/var/templates']));
+        if (! $injector) {
+            $injector = new Injector($module, $_ENV['TMP_DIR']);
+        }
+
+        $this->resource = $injector->getInstance(ResourceInterface::class);
+        parent::setUp();
+    }
+
     public function testSetSurrogateHeader(): void
     {
         $uri = new Uri('app://self/foo');
@@ -31,6 +53,9 @@ class SurrogateKeysTest extends TestCase
         $ro->uri = $uri;
         $etags->setSurrogateHeader($ro);
         $this->assertSame('_foo_ _foo1_ a b _foo2_', $ro->headers[Header::SURROGATE_KEY]);
+
+        $ro = $this->resource->get('page://self/html/page-surrogate-key');
+        $this->assertStringContainsString('page-tag', $ro->headers[Header::SURROGATE_KEY]);
     }
 
     public function testOnePurgeKey(): void
