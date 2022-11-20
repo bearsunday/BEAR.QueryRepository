55b8c26af8ed36fa330c3b90ec8869cbf4126a1c
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-11-20 12:47:17 +0900

    Require ray/rector-ray

--- rector.php
+++ rector.php
@@ -4,21 +4,21 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
+use Rector\Ray\AnnotationBinding\Rector\ClassMethod\AnnotationBindingRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->paths([
+        __DIR__ . '/demo',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
-
+    $rectorConfig->rule(AnnotationBindingRector::class);
     // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
-        SetList::CODE_QUALITY
+        $rectorConfig->sets([
+            LevelSetList::UP_TO_PHP_80,
     ]);
 };
