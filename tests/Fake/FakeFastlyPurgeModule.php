3e2bfe691026f851f8202e4f06067fd2fdb5f402
Yoshitaka Jingu <jingu@bitnauts.co.jp>
2023-09-08 17:01:47 +0900

    Remove unused dependencies

--- tests/Fake/FakeFastlyPurgeModule.php
+++ tests/Fake/FakeFastlyPurgeModule.php
@@ -2,11 +2,8 @@
 
 namespace BEAR\QueryRepository;
 
-use BEAR\QueryRepository\Cdn\FastlyCachePurger;
 use Fastly\Api\PurgeApi;
 use Fastly\Configuration;
-use GuzzleHttp\Client;
-use GuzzleHttp\ClientInterface;
 use Ray\Di\AbstractModule;
 use Ray\Di\Scope;
 
@@ -17,15 +14,8 @@ final class FakeFastlyPurgeModule extends AbstractModule
      */
     protected function configure(): void
     {
-        $this->bind(Configuration::class)->annotatedWith(Configuration::class)->toInstance(
-            Configuration::getDefaultConfiguration()->setApiToken('fakeKey')
-        );
         $this->bind(PurgeApi::class)->toConstructor(FakeFastlyPurgeApi::class, [
             'config' => Configuration::class,
         ])->in(Scope::SINGLETON);
-        $this->bind()->annotatedWith('fastlyServiceId')->toInstance('fakeServiceId');
-        $this->bind()->annotatedWith('fastlyEnableSoftPurge')->toInstance(true);
-        $this->bind(ClientInterface::class)->annotatedWith('fastlyApi')->to(Client::class);
-        $this->bind(PurgerInterface::class)->to(FastlyCachePurger::class);
     }
 }
