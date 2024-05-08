0bf6cc2c192e83466dfa4e8136d62e194c99c213
Kouhei.Sano <apple.x.co@gmail.com>
2024-05-09 08:39:04 +0900

    fix: Change MemcachedAdapter to use

--- src/StorageMemcachedModule.php
+++ src/StorageMemcachedModule.php
@@ -27,7 +27,7 @@ final class StorageMemcachedModule extends AbstractModule
     protected function configure(): void
     {
         $this->install(new Psr6MemcachedModule($this->servers));
-        $this->bind(CacheItemPoolInterface::class)->annotatedWith(EtagPool::class)->toConstructor(MemcachedAdapter::class::class, [
+        $this->bind(CacheItemPoolInterface::class)->annotatedWith(EtagPool::class)->toConstructor(MemcachedAdapter::class, [
             'namespace' => CacheNamespace::class,
             'clientProvider' => 'memcached',
         ]);
