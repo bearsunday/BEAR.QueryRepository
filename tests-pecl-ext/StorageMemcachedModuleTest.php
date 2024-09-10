980b5c490505236d9541e66e1f99f30297c14c62
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-09-11 00:40:25 +0900

    Enhance Memcached module with TagAwareAdapter support
    Updated StorageMemcachedModule to bind TagAwareAdapter for better cache handling. Modified corresponding test to reflect and verify these changes, ensuring compatibility with new annotations and dependencies.

--- tests-pecl-ext/StorageMemcachedModuleTest.php
+++ tests-pecl-ext/StorageMemcachedModuleTest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
+use BEAR\RepositoryModule\Annotation\ResourceObjectPool;
+use BEAR\Resource\AdapterInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Cache\CacheItemPoolInterface;
 use Ray\Di\Injector;
 use Ray\PsrCacheModule\Annotation\Shared;
 use Ray\PsrCacheModule\MemcachedAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapterInterface;
 
 class StorageMemcachedModuleTest extends TestCase
 {
@@ -16,7 +20,16 @@ class StorageMemcachedModuleTest extends TestCase
     {
         // @see http://php.net/manual/en/memcached.addservers.php
         $servers = 'mem1.domain.com:11211:33,mem2.domain.com:11211:67';
-        $cache = (new Injector(new StorageMemcachedModule($servers)))->getInstance(CacheItemPoolInterface::class, Shared::class);
-        $this->assertInstanceOf(MemcachedAdapter::class, $cache);
+        $module = new QueryRepositoryModule();
+        $module->override(new StorageMemcachedModule($servers));
+        $injector = (new Injector($module, __DIR__ . '/tmp'));
+        $storage = $injector->getInstance(ResourceStorageInterface::class);
+        $this->assertInstanceOf(ResourceStorage::class, $storage);
+        $adapter = $injector->getInstance(TagAwareAdapterInterface::class, ResourceObjectPool::class);
+        $this->assertInstanceOf(TagAwareAdapter::class, $adapter);
+        $roPool = $injector->getInstance(\Symfony\Component\Cache\Adapter\AdapterInterface::class, ResourceObjectPool::class);
+        $etagPool = $injector->getInstance(\Symfony\Component\Cache\Adapter\AdapterInterface::class, ResourceObjectPool::class);
+        $this->assertInstanceOf(MemcachedAdapter::class, $roPool);
+        $this->assertInstanceOf(MemcachedAdapter::class, $etagPool);
     }
 }
