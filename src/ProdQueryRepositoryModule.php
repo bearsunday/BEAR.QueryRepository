00543f5ac77652d7bca703cb4b3889be51aee8fe
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-09-11 03:30:44 +0900

    Add local cache binding for ResourceObjectPool
    Bind the ResourceObjectPool annotation to use the LocalCacheProvider, enhancing caching capabilities. This change aims to improve performance by utilizing local cache mechanisms like APCu or filesystem storage.

--- src/ProdQueryRepositoryModule.php
+++ src/ProdQueryRepositoryModule.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace BEAR\QueryRepository;
 
 use BEAR\RepositoryModule\Annotation\KnownTagTtl;
+use BEAR\RepositoryModule\Annotation\ResourceObjectPool;
 use Ray\Di\AbstractModule;
+use Ray\PsrCacheModule\LocalCacheProvider;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 final class ProdQueryRepositoryModule extends AbstractModule
 {
@@ -16,6 +19,8 @@ final class ProdQueryRepositoryModule extends AbstractModule
      */
     protected function configure(): void
     {
+        // Bind ResourceObjectPool to local cache (APCu or File)
+        $this->bind(AdapterInterface::class)->annotatedWith(ResourceObjectPool::class)->toProvider(LocalCacheProvider::class);
         // boost the performance of symfony/cache
         $this->bind()->annotatedWith(KnownTagTtl::class)->toInstance(0.15);
     }
