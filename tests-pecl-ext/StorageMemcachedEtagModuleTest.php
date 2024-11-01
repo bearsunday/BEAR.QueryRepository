2f1c0b183dc93b93180a7e2739fa7be4af8c1da9
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-01 23:40:47 +0900

    Add test for invalid server string in Memcached Etag Module
    A new test case `testInvalidServerString` was added to ensure that passing an invalid server string to `StorageMemcachedEtagModule` triggers a `CacheException`. This change helps validate proper error handling for incorrect configurations.

--- tests-pecl-ext/StorageMemcachedEtagModuleTest.php
+++ tests-pecl-ext/StorageMemcachedEtagModuleTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
+use _PHPStan_c875e8309\Nette\InvalidArgumentException;
 use BEAR\RepositoryModule\Annotation\EtagPool;
 use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheException;
 use Psr\Cache\CacheItemPoolInterface;
 use Ray\Di\Injector;
 use Ray\PsrCacheModule\Annotation\Shared;
@@ -19,5 +21,15 @@ class StorageMemcachedEtagModuleTest extends TestCase
         $servers = 'mem1.domain.com:11211:33,mem2.domain.com:11211:67';
         $cache = (new Injector(new StorageMemcachedEtagModule($servers)))->getInstance(CacheItemPoolInterface::class, EtagPool::class);
         $this->assertInstanceOf(MemcachedAdapter::class, $cache);
+    }
+
+    public function testInvalidServerString(): void
+    {
+        $this->expectException(CacheException::class);
+        $servers = 'invalid:server:string';
+        $cache = (new Injector(new StorageMemcachedEtagModule($servers)))
+            ->getInstance(CacheItemPoolInterface::class, EtagPool::class);
+        assert($cache instanceof CacheItemPoolInterface);
+        $cache->clear();
     }
 }
