a5caad590bedb770498b1b11ae885c030b0138e2
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-24 09:07:44 +0900

    Refactor unlink function and add phpstan annotation
    Update `unlink` function to require a string type for $path parameter, ensuring type safety. Additionally, add @phpstan-ignore-line for PHP 8 to suppress static analysis warnings in `ServiceLocator` condition.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Koriym\Attributes\AttributeReader;
 use Ray\ServiceLocator\ServiceLocator;
 
-$unlink = static function ($path) use (&$unlink): void {
+$unlink = static function (string $path) use (&$unlink): void {
     foreach ((array) glob(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '*') as $f) {
         $file = (string) $f;
         is_dir($file) ? $unlink($file) : unlink($file);
@@ -15,6 +15,6 @@ $unlink = static function ($path) use (&$unlink): void {
 $unlink(__DIR__ . '/tmp');
 
 // no annotation in PHP 8
-if (PHP_MAJOR_VERSION >= 8) {
+if (PHP_MAJOR_VERSION >= 8) { // @phpstan-ignore-line
     ServiceLocator::setReader(new AttributeReader());
 }
