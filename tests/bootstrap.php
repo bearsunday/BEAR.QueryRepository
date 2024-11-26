6213d6d7f9004eaaa319c84a0848d04bc2e1efa6
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-26 19:20:22 +0900

    fixup! Simplify test bootstrap file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Koriym\Attributes\AttributeReader;
 use Ray\ServiceLocator\ServiceLocator;
 
-array_map('unlink', glob(__DIR__ . '/tests/tmp/*.php'));
+array_map('unlink', glob(__DIR__ . '/tests/tmp/*.php')); // @phpstan-ignore-line
 
 ServiceLocator::setReader(new AttributeReader());
