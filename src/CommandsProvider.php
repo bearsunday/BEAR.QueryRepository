01aa1cecd9af317db30563c19804ba8d450ab97f
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-01-06 10:21:17 +0900

    Update dependencies and refactor `CommandsProvider` constructor
    Upgraded multiple dependencies in `composer.lock` to include the latest versions. Updated `CommandsProvider` to replace `QueryRepositoryInterface` and `ResourceInterface` with specific command dependencies for better clarity and modular design.

--- src/CommandsProvider.php
+++ src/CommandsProvider.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 namespace BEAR\QueryRepository;
 
-use BEAR\Resource\ResourceInterface;
 use Ray\Di\ProviderInterface;
 
 /** @implements ProviderInterface<array<CommandInterface>> */
 final class CommandsProvider implements ProviderInterface
 {
     public function __construct(
-        private readonly QueryRepositoryInterface $repository,
-        private readonly ResourceInterface $resource,
+        private readonly RefreshSameCommand $refreshSameCommand,
+        private readonly RefreshAnnotatedCommand $refreshAnnotatedCommand,
     ) {
     }
 
@@ -22,8 +21,8 @@ final class CommandsProvider implements ProviderInterface
     public function get()
     {
         return [
-            new RefreshSameCommand($this->repository, new MatchQuery()),
-            new RefreshAnnotatedCommand($this->repository, $this->resource),
+            $this->refreshSameCommand,
+            $this->refreshAnnotatedCommand,
         ];
     }
 }
