1d37b667d7076dad6a038ad0d951925d9bb63457
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-10-21 08:57:34 +0900

    fix: Add getVersion() method to fake Memcached class
    Symfony Cache MemcachedAdapter requires Memcached::getVersion() to return
    version > 3.1.5. The fake implementation now returns '3.2.0' to satisfy
    this requirement when the actual Memcached extension is not available.

--- tests/Fake/Memcached.php
+++ tests/Fake/Memcached.php
@@ -5,5 +5,11 @@ if (! class_exists(\Memcached::class)) {
         public function addServers(array $servers)
         {
         }
+
+        public function getVersion()
+        {
+            // Return a version that satisfies Symfony Cache requirement (> 3.1.5)
+            return ['localhost' => '3.2.0'];
+        }
     }
 }
