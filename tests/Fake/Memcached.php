f887b05320021a6df4022027e51e054e2b3abed5
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-10-21 09:13:13 +0900

    fix: Skip Memcached tests when extension is not available or too old
    - Add extension_loaded and version checks to Memcached-dependent tests
    - Skip tests if Memcached extension < 2.2.0 (required by Symfony Cache)
    - Revert Fake Memcached getVersion() addition (not needed with test skipping)
    - Fix phpcs.xml: Remove non-existent DNFTypeHintFormat sniff
    
    This allows PHP 8.5 Windows CI to pass where Memcached extension is not yet available.

--- tests/Fake/Memcached.php
+++ tests/Fake/Memcached.php
@@ -5,11 +5,5 @@ if (! class_exists(\Memcached::class)) {
         public function addServers(array $servers)
         {
         }
-
-        public function getVersion()
-        {
-            // Return a version that satisfies Symfony Cache requirement (> 3.1.5)
-            return ['localhost' => '3.2.0'];
-        }
     }
 }
