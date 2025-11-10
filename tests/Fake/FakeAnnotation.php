9edb9955270a6346e033be74a6c2c58a76ccdd6b
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-11-10 19:13:14 +0900

    refactor: Remove legacy annotation docblock from FakeAnnotation

--- tests/Fake/FakeAnnotation.php
+++ tests/Fake/FakeAnnotation.php
@@ -1,12 +1,11 @@
 <?php
+
+declare(strict_types=1);
+
 namespace BEAR\QueryRepository;
 
 use Attribute;
 
-/**
- * @Annotation
- * @Target("METHOD")
- */
 #[Attribute(Attribute::TARGET_METHOD)]
 final class FakeAnnotation
 {
